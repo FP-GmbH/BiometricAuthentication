6692e7fcbf13ec64a396f751066c99eb96667234
Marvin Scharger <msc@fp.de>
2021-04-29 12:29:33 +0200

    [MAINT] adjust deployment target (iOS 11) for JOYCE compatibility

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "BiometricAuthentication",
     platforms: [
-        .iOS(.v12),
+        .iOS(.v11),
     ],
     products: [
         .library(name: "BiometricAuthentication", targets: ["BiometricAuthentication"])
